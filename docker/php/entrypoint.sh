f69db87758b1167319422ff06e85f4b4913f9317
Balboa <you@example.com>
2025-12-05 03:46:48 +0700

    Изменения в конфигурации docker

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 if [ ! -f "/var/www/z-test-yugrin/.import_done" ]; then
     sleep 20
     if [ -f "/var/www/z-test-yugrin/test_task_data.csv" ]; then
-        chmod -R 777 /var
-        php bin/console doctrine:migrations:migrate --no-interaction || true
-        php bin/console app:user:create-test || true
-        php bin/console app:tenders:init || true
+        chown www-data:www-data -R var/
+        su -s /bin/sh www-data -c "php bin/console doctrine:migrations:migrate --no-interaction" || true
+        su -s /bin/sh www-data -c "php bin/console app:user:create-test" || true
+        su -s /bin/sh www-data -c "php bin/console app:tenders:init" || true
         echo "done" > /var/www/z-test-yugrin/.import_done
     fi
 fi
